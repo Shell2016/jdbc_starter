5632468fa81e48f00848d53dfbc235a9d74a9b37
Shell2016 <bvg2003@bk.ru>
2022-06-29 13:13:36 +0300

    preparedStatement lesson

--- src/ru/michaelshell/JdbcRunner.java
+++ src/ru/michaelshell/JdbcRunner.java
@@ -1,13 +1,35 @@
 package ru.michaelshell;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
 
-        try (Connection connection = ConnnectionManager.open()) {
-            System.out.println(connection.getTransactionIsolation());
+        System.out.println(getFlightsBetween(LocalDate.of(2020, 9, 9).atStartOfDay(), LocalDateTime.now()));
+
+    }
+
+    private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) throws SQLException {
+        String sql = """
+                SELECT *
+                FROM flight
+                WHERE departure_date BETWEEN ? AND ?
+                """;
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getLong("id"));
+            }
         }
+        return result;
     }
 }
